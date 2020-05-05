36e359cab86d41ac9105f45f4fad0fab361ee40b
isaac stott <isaac.cloyd@gmail.com>
2020-05-04 19:13:33 -0600

    got code up, met mvp, styled

---
+++ src/components/TodoForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+  // Constructor with state
+  constructor() {
+    super();
+    this.state = {
+      item: ''
+    };
+  }
+
+  handleChanges = e => {
+    // update state with each keystroke
+    this.setState({
+      item: e.target.value
+    });
+    console.log(this.state)
+  };
+
+  // class property to submit form
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.addTask(this.state.item);
+    this.setState({
+      item: ''
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="item"
+          value={this.state.item}
+          placeholder= " ...to-do"
+          onChange={this.handleChanges}
+        />
+        <button>Add to-do</button>
+      </form>
+    );
+  }
+}
+
+export default TodoForm;
